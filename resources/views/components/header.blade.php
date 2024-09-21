5efa792c1870a41c8c86d0d8fa05bf8bc0fba930
javisotieno <javisotieno@gmail.com>
2024-09-21 06:32:06 +0300

    home shop

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -60,7 +60,7 @@
                             <div class="collapse navbar-toggleable-xs" id="default-header">
                                 <nav>
                                     <ul id="menu-main-menu" class="nav nav-inline yamm">
-                                        {{-- <li class="menu-item"><a title="Home" href="/">Home</a></li>      --}}
+                                        <li class="menu-item"><a title="Home" href="/">Home</a></li>     
                                         <li class="menu-item"><a title="Shop" href="/shop">Shop</a></li> 
                                         {{-- <li class="menu-item"><a title="About" href="/about">About Us</a></li> --}}
                                         <li class="menu-item"><a title="Contact" href="#">Contact Us</a></li>                                     
