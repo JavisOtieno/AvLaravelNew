f9ddc5ec384c26f2b4b651361f8c4cb52f938e83
javisotieno <javisotieno@gmail.com>
2024-09-21 07:40:43 +0300

    email and phone

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -68,12 +68,12 @@
                                                 <div class="wpb_wrapper">
                                                     <div class="wpb_text_column wpb_content_element ">
                                                         <div class="wpb_wrapper">
-                                                            <h2 class="contact-page-title">Our Store</h2>
-                                                            <address>17 Princess Road<br>
-                                                                London, Greater London<br>
-                                                                NW1 8JR, UK
+                                                            <h2 class="contact-page-title">Our Contact</h2>
+                                                            <address>Email: {{request()->get('user')['email']}}<br>
+                                                                Phone: {{request()->get('user')['phone']}}<br>
+                                                                
                                                             </address>
-                                                            <h3>Hours of Operation</h3>
+                                                            {{-- <h3>Hours of Operation</h3>
                                                             <ul class="list-unstyled operation-hours inner-right-md">
                                                                 <li class="clearfix"><span class="day">Monday:</span><span class="pull-right flip hours">12-6 PM</span></li>
                                                                 <li class="clearfix"><span class="day">Tuesday:</span><span class="pull-right flip hours">12-6 PM</span></li>
@@ -84,7 +84,7 @@
                                                                 <li class="clearfix"><span class="day">Sunday</span><span class="pull-right flip hours">Closed</span></li>
                                                             </ul>
                                                             <h3>Careers</h3>
-                                                            <p class="inner-right-md">If you’re interested in employment opportunities at Electro, please email us: <a href="mailto:contact@yourstore.com">contact@yourstore.com</a></p>
+                                                            <p class="inner-right-md">If you’re interested in employment opportunities at Electro, please email us: <a href="mailto:contact@yourstore.com">contact@yourstore.com</a></p> --}}
                                                         </div>
                                                     </div>
                                                 </div>
@@ -96,11 +96,11 @@
                                                     <div class="wpb_text_column wpb_content_element ">
                                                         <div class="wpb_wrapper">
                                                             <h2 class="contact-page-title">Our Info</h2>
-                                                            <address>17 Princess Road<br>
-                                                                London, Greater London<br>
-                                                                NW1 8JR, UK
+                                                            <address>CBD<br>
+                                                                Nairobi<br>
+                                                                Kenya
                                                             </address>
-                                                            <h3>Hours of Operation</h3>
+                                                            {{-- <h3>Hours of Operation</h3>
                                                             <ul class="list-unstyled operation-hours inner-right-md">
                                                                 <li class="clearfix"><span class="day">Monday:</span><span class="pull-right flip hours">12-6 PM</span></li>
                                                                 <li class="clearfix"><span class="day">Tuesday:</span><span class="pull-right flip hours">12-6 PM</span></li>
@@ -111,7 +111,7 @@
                                                                 <li class="clearfix"><span class="day">Sunday</span><span class="pull-right flip hours">Closed</span></li>
                                                             </ul>
                                                             <h3>Careers</h3>
-                                                            <p class="inner-right-md">If you’re interested in employment opportunities at Electro, please email us: <a href="mailto:contact@yourstore.com">contact@yourstore.com</a></p>
+                                                            <p class="inner-right-md">If you’re interested in employment opportunities at Electro, please email us: <a href="mailto:contact@yourstore.com">contact@yourstore.com</a></p> --}}
                                                         </div>
                                                     </div>
                                                 </div>
