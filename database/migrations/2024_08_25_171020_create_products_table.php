dab2bed4b9759041e9189f4df6b3e1f23cb82038
javisotieno <javisotieno@gmail.com>
2024-10-10 17:34:42 +0300

    new category id on product migration

--- database/migrations/2024_08_25_171020_create_products_table.php
+++ database/migrations/2024_08_25_171020_create_products_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('highlights');
             $table->string('image')->nullable();
             $table->string('status');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->longText('description');
             $table->timestamps();
         });
