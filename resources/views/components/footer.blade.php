b4921b9edf7ba03608b989189e7c18d2a00be6ba
javisotieno <javisotieno@gmail.com>
2024-10-21 13:33:11 +0300

    test footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -330,22 +330,19 @@
     <div class="footer-newsletter">
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                    <h5 class="newsletter-title">Free Delivery within Nairobi CBD</h5>
-                    {{-- <span class="newsletter-marketing-text">...and receive <strong>$20 coupon for first shopping</strong></span> --}}
+                <div class="col-xs-12 col-sm-7">
+                    <h5 class="newsletter-title">Sign up to Newsletter</h5>
+                    <span class="newsletter-marketing-text">...and receive <strong>$20 coupon for first shopping</strong></span>
                 </div>
-                <div class="col-xs-12 col-sm-6">
-                    
-                        <h5 class="newsletter-title">Delivery Countrywide at affordable rates</h5>
-                    
-                    {{-- <form>
+                <div class="col-xs-12 col-sm-5">
+                    <form>
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Enter your email address">
                             <span class="input-group-btn">
                                 <button class="btn btn-secondary" type="button">Sign Up</button>
                             </span>
                         </div>
-                    </form> --}}
+                    </form>
                 </div>
             </div>
         </div>
