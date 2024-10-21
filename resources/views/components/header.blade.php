be2406cf64b2668eae6a2c17e9046948824ac355
javisotieno <javisotieno@gmail.com>
2024-10-21 11:54:17 +0300

    remove categories on search

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -656,7 +656,7 @@
                         <label class="sr-only screen-reader-text" for="search">Search for:</label>
                         <div class="input-group">
                             <input type="text" id="search" class="form-control search-field" dir="ltr" value="" name="s" placeholder="Search for products" />
-                            <div class="input-group-addon search-categories">
+                            {{-- <div class="input-group-addon search-categories">
                                 <select name='product_cat' id='product_cat' class='postform resizeselect' >
                                     <option value='0' selected='selected'>All Categories</option>
                                     <option class="level-0" value="laptops-laptops-computers">Laptops</option>
@@ -685,7 +685,7 @@
                                     <option class="level-0" value="wearables">Wearables</option>
                                     <option class="level-0" value="pendrives">Pendrives</option>
                                 </select>
-                            </div>
+                            </div> --}}
                             <div class="input-group-btn">
                                 <input type="hidden" id="search-param" name="post_type" value="product" />
                                 <button type="submit" class="btn btn-secondary"><i class="ec ec-search"></i></button>
