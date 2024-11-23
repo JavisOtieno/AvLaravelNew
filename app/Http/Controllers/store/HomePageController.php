5a6756f543a7b6c493481fdb1044f547c0ff5477
javisotieno <javisotieno@gmail.com>
2024-11-23 11:43:40 +0300

    product order successfully added

--- app/Http/Controllers/store/HomePageController.php
+++ app/Http/Controllers/store/HomePageController.php
@@ -111,7 +111,7 @@ class HomePageController extends Controller
                 "status" => "success");
     
             // return response()->json($orderStatus);
-            return redirect('/buynow')->with('error', 'Order Added Successfully');
+            return redirect('/buynow/' . $product->id)->with('error', 'Order Added Successfully');
           
     }
 
