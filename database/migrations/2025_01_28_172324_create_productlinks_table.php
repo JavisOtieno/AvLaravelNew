680a3909097390aece9c21fcda81bd98853cf6a2
javisotieno <javisotieno@gmail.com>
2025-02-01 13:03:34 +0300

    added name and product link to  productlinks table

--- database/migrations/2025_01_28_172324_create_productlinks_table.php
+++ database/migrations/2025_01_28_172324_create_productlinks_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('productlinks', function (Blueprint $table) {
             $table->id();
+            $table->string('product_id')->nullable();
+            $table->string('name');
             $table->string('link');
             $table->unsignedBigInteger('price');
             $table->timestamps();
