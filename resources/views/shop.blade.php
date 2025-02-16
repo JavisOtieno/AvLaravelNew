941bcf07f706cbcf11db18a36c7ffd214ca4ec4f
JavisOtieno <javisotieno@gmail.com>
2025-02-16 03:22:46 +0300

    absolute url on category

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -3150,7 +3150,7 @@
                                         <li class="product_cat"><span class="show-all-cat-dropdown">Show All Categories</span>
                                             <ul>
                                                 @foreach ($maincategories as $category)
-                                                <li class="cat-item"><a href="category/{{$category['name']}}">{{ucfirst($category['title'])}}</a> </li>
+                                                <li class="cat-item"><a href="/category/{{$category['name']}}">{{ucfirst($category['title'])}}</a> </li>
                                                 {{-- <span class="count">(0)</span> --}}
 
                                                 
