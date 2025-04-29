e57f729fd0a358168afd28fa7876693cd9b117f2
javisotieno <javisotieno@gmail.com>
2025-04-29 10:20:44 +0300

    main block added

--- resources/views/auth/forgotPassword.blade.php
+++ resources/views/auth/forgotPassword.blade.php
@@ -86,6 +86,13 @@
         'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
     -->
     <div id="page-container">
+              <!-- Main Container -->
+      <main id="main-container">
+        <!-- Page Content -->
+        <div class="hero-static d-flex align-items-center">
+          <div class="content">
+            <div class="row justify-content-center push">
+              <div class="col-md-8 col-lg-6 col-xl-4">
 {{-- <main class="login-form">
   <div class="cotainer">
       <div class="row justify-content-center">
@@ -194,6 +201,15 @@
     </div>
 </div>
 {{-- @endsection --}}
+</div>
+</div>
+<div class="fs-sm text-muted text-center">
+  <strong>Fave</strong> &copy; <span data-toggle="year-copy"></span>
+</div>
+</div>
+</div>
+<!-- END Page Content -->
+</main>
 
     <!-- END Main Container -->
 </div>
