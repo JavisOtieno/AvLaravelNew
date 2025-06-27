2cb0e7918f1ec3c7eede9bd453eade1c52c33e91
javisotieno <javisotieno@gmail.com>
2025-06-27 19:08:05 +0300

    customer on orders

--- app/Http/Controllers/api/DashboardController.php
+++ app/Http/Controllers/api/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
 
     public function dashboard(){
         $user_id = auth()->user()->id;
-        $orders = Order::orderBy('id', 'desc')->where('user_id', $user_id)->take(5)->get();
+        $orders = Order::with('customer')->orderBy('id', 'desc')->where('user_id', $user_id)->take(5)->get();
         $products = Product::orderBy('id', 'desc')->take(5)->get();
         $allorders = Order::where('user_id', $user_id)->count(); 
         $processingorders = Order::where('user_id', $user_id)->where('status','processing')->count();
