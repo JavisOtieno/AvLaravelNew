8ddf29907acde27572a02bbf975569b63af43d67
javisotieno <javisotieno@gmail.com>
2025-07-01 14:43:15 +0300

    screen desktop icon

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -662,7 +662,7 @@
               </li>
                <li class="nav-main-item">
                 <a class="nav-main-link" target="_blank" href="{{ 'https://'.auth()->user()->websitename.'.av.ke' }}">
-                  <i class="nav-main-link-icon si si-wallet"></i>
+                  <i class="nav-main-link-icon si si-screen-desktop"></i>
                   <span class="nav-main-link-name">Website</span>
                 </a>
               </li>
