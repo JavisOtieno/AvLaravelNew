581c9d03961ccf11dea7a38e12541af498df5ffe
javisotieno <javisotieno@gmail.com>
2025-07-01 14:14:10 +0300

    website name on navbar

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -660,6 +660,12 @@
                   <span class="nav-main-link-name">Customers</span>
                 </a>
               </li>
+               <li class="nav-main-item">
+                <a class="nav-main-link" target="_blank" href="{{ 'https://'.auth()->user()->websitename.'.av.ke' }}">
+                  <i class="nav-main-link-icon si si-users"></i>
+                  <span class="nav-main-link-name">Website</span>
+                </a>
+              </li>
 
             
 
