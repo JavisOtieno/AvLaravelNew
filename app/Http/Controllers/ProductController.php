952cfc0bdc58e8d98f439d470f6676321f1063dd
javisotieno <javisotieno@gmail.com>
2025-07-10 17:16:29 +0300

    update product prices on api/productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,29 +24,7 @@ class ProductController extends Controller
         return view('admin.products',compact('products','categories'));
     }
 
-    public function updateProductPrices(){
-        // $products = Product::all();
-        $productlinks =  Productlink::with('product')
-            ->whereNotNull('product_id')
-            ->orderBy('id', 'desc')
-            // ->take(20)
-            ->get(); // Eager load to optimize performance
-
-        foreach ($productlinks as $productlink) {
-            $product = $productlink->product;
-
-            if ($product && isset($productlink->price)) {
-                $formerprice = $product->price;
-                $product->price = $productlink->price;
-                $product->save();
-
-                echo "✅ Updated Product ID {$product->id} Previously {$formerprice} with Price {$productlink->price}<br/>";
-            } else {
-                echo "❌ No valid productLink or missing price for Product ID {$product->id}<br/>";
-            }
-        }
-        
-    }
+   
 
 
 
--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Models\Product;
+use App\Models\Productlink;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -26,4 +27,27 @@ class ProductController extends Controller
             $product = Product::with('category')->find($id);
             return response()->json(['product'=>$product]);   
         }
+         public function updateProductPrices(){
+        // $products = Product::all();
+        $productlinks =  Productlink::with('product')
+            ->whereNotNull('product_id')
+            ->orderBy('id', 'desc')
+            // ->take(20)
+            ->get(); // Eager load to optimize performance
+
+        foreach ($productlinks as $productlink) {
+            $product = $productlink->product;
+
+            if ($product && isset($productlink->price)) {
+                $formerprice = $product->price;
+                $product->price = $productlink->price;
+                $product->save();
+
+                echo "✅ Updated Product ID {$product->id} Previously {$formerprice} with Price {$productlink->price}<br/>";
+            } else {
+                echo "❌ No valid productLink or missing price for Product ID {$product->id}<br/>";
+            }
+        }
+        
+    }
 }
