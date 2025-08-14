f83500945bdfc670efd7650d19d549b10b96bf4b
javisotieno <javisotieno@gmail.com>
2025-08-14 19:02:08 +0300

    without header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,9 @@
-@include('components/header')
+{{-- @include('components/header') --}}
+@include('components/topbarheader')
+
+{{-- <body class="left-sidebar"> --}}
 
 @yield('content')
 
-
+@include('components/footer')
 {{-- test --}}
