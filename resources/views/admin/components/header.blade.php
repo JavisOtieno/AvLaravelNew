fa178adde1496cd0dcc25c38b5860f163544c7bb
javisotieno <javisotieno@gmail.com>
2025-08-18 11:57:57 +0300

    remove target blank on website click

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -661,7 +661,7 @@
                 </a>
               </li>
                <li class="nav-main-item">
-                <a class="nav-main-link" target="_blank" href="/adminwebsite">
+                <a class="nav-main-link" href="/adminwebsite">
                   <i class="nav-main-link-icon si si-screen-desktop"></i>
                   <span class="nav-main-link-name">Website</span>
                 </a>
